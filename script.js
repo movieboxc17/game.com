4906c47415ef315a3eecd9102816c02f78ddfa22
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-03-22 16:42:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let messages = [];
 
 const chatContainer = document.getElementById("chat-container");
 const sendButton = document.getElementById("send-button");
+const ws = new WebSocket("ws://localhost:8080");
 
 function displayMessages() {
   let chatContent = "";
@@ -17,6 +18,8 @@ function displayMessages() {
       const index = button.getAttribute("data-index");
       messages.splice(index, 1);
       displayMessages();
+      // Send message deletion to server
+      ws.send(JSON.stringify({ type: "delete", index: index }));
     });
   });
 }
@@ -26,4 +29,17 @@ sendButton.addEventListener("click", function () {
   messages.push(userMessage);
   displayMessages();
   document.getElementById("user-message").value = "";
+  // Send new message to server
+  ws.send(JSON.stringify({ type: "message", message: userMessage }));
+});
+
+ws.addEventListener("message", function (event) {
+  const data = JSON.parse(event.data);
+  if (data.type === "message") {
+    messages.push(data.message);
+    displayMessages();
+  } else if (data.type === "delete") {
+    messages.splice(data.index, 1);
+    displayMessages();
+  }
 });
