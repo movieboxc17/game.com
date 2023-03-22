803ba53ddde11a17a2fed1f57f90a8190eb6fbc0
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-03-22 10:40:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    if (username == "" || password == "") {
-      var errorMessage = document.createElement("p");
-      errorMessage.className = "error-message";
-      errorMessage.textContent = "Please enter both username and password.";
-      document.getElementsByTagName("form")[0].appendChild(errorMessage);
-    } else if (username != "example" || password != "password") {
-      var errorMessage = document.createElement("p");
-      errorMessage.className = "error-message";
-      errorMessage.textContent = "Incorrect username or password.";
-      document.getElementsByTagName("form")[0].appendChild(errorMessage);
-    } else {
-      alert("Login successful!");
-    }
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+var player = {
+  x: 50,
+  y: height - 100,
+  width: 50,
+  height: 50,
+  speed: 5
+};
+
+function update() {
+  if (keys["ArrowLeft"]) {
+    player.x -= player.speed;
   }
-  
+  if (keys["ArrowRight"]) {
+    player.x += player.speed;
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillStyle = "#ff6600";
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function loop() {
+  update();
+  draw();
+  window.requestAnimationFrame(loop);
+}
+
+var keys = {};
+window.addEventListener("keydown", function(event) {
+  keys[event.code] = true;
+});
+window.addEventListener("keyup", function(event) {
+  keys[event.code] = false;
+});
+
+loop();
