72f6f7979f7dadcea0b016eb2c21eed5f324fb29
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-03-22 16:01:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let messages = [];
 
+const chatContainer = document.getElementById("chat-container");
+const sendButton = document.getElementById("send-button");
+
+function displayMessages() {
+  let chatContent = "";
+  messages.forEach(function (message) {
+    chatContent += "<p>" + message + "</p>";
+  });
+  chatContainer.innerHTML = chatContent;
+}
+
+sendButton.addEventListener("click", function () {
+  const userMessage = document.getElementById("user-message").value;
+  messages.push(userMessage);
+  displayMessages();
+  document.getElementById("user-message").value = "";
+});
