6450f81a6f6f6157500be9ee479dd2b6d012ef89
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-03-22 16:21:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ const sendButton = document.getElementById("send-button");
 
 function displayMessages() {
   let chatContent = "";
-  messages.forEach(function (message) {
-    chatContent += "<p>" + message + "</p>";
+  messages.forEach(function (message, index) {
+    chatContent += "<p>" + message + "<button class='delete-button' data-index='" + index + "'>Delete</button></p>";
   });
   chatContainer.innerHTML = chatContent;
+
+  // Add event listeners to delete buttons
+  const deleteButtons = document.getElementsByClassName("delete-button");
+  Array.from(deleteButtons).forEach(function (button) {
+    button.addEventListener("click", function () {
+      const index = button.getAttribute("data-index");
+      messages.splice(index, 1);
+      displayMessages();
+    });
+  });
 }
 
 sendButton.addEventListener("click", function () {
