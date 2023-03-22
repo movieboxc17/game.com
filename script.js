45583f987882fd9d338d31143b0fa3097d51e4c8
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-03-22 13:19:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const messageInput = document.querySelector('.message-form input[type="text"]');
 const sendButton = document.querySelector('.message-form button[type="submit"]');
 const messagesDiv = document.querySelector('.messages');
 
+// Initialize messages array
+let messages = [];
+
 // Event listener for submitting message
 messageForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -22,15 +25,25 @@ function sendMessage() {
   if (message) {
     const username = 'JohnDoe'; // You can change this to a dynamic value
     const timestamp = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('message');
-    messageElement.innerHTML = `
-      <span class="username">${username}</span>
-      <span class="timestamp">${timestamp}</span>
-      <p class="content">${message}</p>
-    `;
-    messagesDiv.appendChild(messageElement);
+    const messageObj = { username, timestamp, message };
+    messages.push(messageObj);
+    displayMessages();
     messageInput.value = '';
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
   }
+}
+
+// Function to display messages
+function displayMessages() {
+  messagesDiv.innerHTML = '';
+  messages.forEach((message) => {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.innerHTML = `
+      <span class="username">${message.username}</span>
+      <span class="timestamp">${message.timestamp}</span>
+      <p class="content">${message.message}</p>
+    `;
+    messagesDiv.appendChild(messageElement);
+  });
 }
