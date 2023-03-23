45344683131ca6a0e6cb97871e711385a5386dbf
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-03-23 09:22:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,14 @@
-let messages = [];
+var windowCount = 0;
 
-const chatContainer = document.getElementById("chat-container");
-const sendButton = document.getElementById("send-button");
-const ws = new WebSocket("ws://localhost:8080/");
-
-function displayMessages() {
-  let chatContent = "";
-  messages.forEach(function (message, index) {
-    chatContent += "<p>" + message + "<button class='delete-button' data-index='" + index + "'>Delete</button></p>";
-  });
-  chatContainer.innerHTML = chatContent;
-
-  // Add event listeners to delete buttons
-  const deleteButtons = document.getElementsByClassName("delete-button");
-  Array.from(deleteButtons).forEach(function (button) {
-    button.addEventListener("click", function () {
-      const index = button.getAttribute("data-index");
-      messages.splice(index, 1);
-      displayMessages();
-      // Send message deletion to server
-      ws.send(JSON.stringify({ type: "delete", index: index }));
-    });
-  });
+function openWindow() {
+	windowCount++;
+	var windowElem = document.createElement("div");
+	windowElem.classList.add("window");
+	windowElem.innerHTML = "<h2>Window " + windowCount + "</h2>";
+	document.body.appendChild(windowElem);
 }
 
-sendButton.addEventListener("click", function () {
-  const userMessage = document.getElementById("user-message").value;
-  messages.push(userMessage);
-  displayMessages();
-  document.getElementById("user-message").value = "";
-  // Send new message to server
-  ws.send(JSON.stringify({ type: "message", message: userMessage }));
-});
-
-ws.addEventListener("message", function (event) {
-  const data = JSON.parse(event.data);
-  if (data.type === "message") {
-    messages.push(data.message);
-    displayMessages();
-  } else if (data.type === "delete") {
-    messages.splice(data.index, 1);
-    displayMessages();
-  }
-});
+var windowBtns = document.querySelectorAll(".window-btn");
+for (var i = 0; i < windowBtns.length; i++) {
+	windowBtns[i].addEventListener("click", openWindow);
+}
