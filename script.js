56c7bb9b3250e85d238f6c5c5024ad4c0a52cd23
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-04-13 13:51:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 const browserWindow = document.getElementById('browser-window');
 
 searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  loadUrl(searchInput.value);
+});
+
+searchButton.addEventListener('click', (event) => {
   event.preventDefault();
   loadUrl(searchInput.value);
 });
