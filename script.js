a6090956705d4bd1afc558148ec5487fd65b430b
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-04-13 13:26:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// Get the button element
-const button = document.querySelector('button');
+const backBtn = document.getElementById('back-btn');
+const forwardBtn = document.getElementById('forward-btn');
+const searchBar = document.getElementById('search-bar');
+const searchBtn = document.getElementById('search-btn');
+const browserWindow = document.getElementById('browser-window');
 
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-  // Get the input element
-  const input = document.querySelector('input');
-  
-  // Get the value of the input
-  const url = input.value;
-  
-  // Load the URL in a new window
-  window.open(url, '_blank');
+backBtn.addEventListener('click', () => {
+  browserWindow.contentWindow.history.back();
 });
+
+forwardBtn.addEventListener('click', () => {
+  browserWindow.contentWindow.history.forward();
+});
+
+searchBar.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    loadUrl(searchBar.value);
+  }
+});
+
+searchBtn.addEventListener('click', () => {
+  loadUrl(searchBar.value);
+});
+
+function loadUrl(url) {
+  if (!url.startsWith('http://') && !url.startsWith('https://')) {
+    url = 'https://' + url;
+  }
+  browserWindow.src = url;
+}
