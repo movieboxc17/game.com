9efa2c612baa161deaa06ecc584d3096163967e7
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-04-19 13:14:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ buttons.forEach(button => {
     alert('Item added to cart!');
   });
 });
+
+<!-- PushAlert Onsite Messaging -->
+<script type="text/javascript">
+    (function(d, t) {
+        var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+        g.src = "https://cdn.inwebr.com/inwebr_d218df97aae8365c4bb3d5e5afc28204.js";
+        s.parentNode.insertBefore(g, s);
+    }(document, "script"));
+</script>
+<!-- End PushAlert Onsite Messaging -->
