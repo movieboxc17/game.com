c37e69785a0214421c6f7d3a53218464901b677f
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-05-10 10:27:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// app.js
 
-const downloadBtn = document.getElementById('download-btn');
-
-downloadBtn.addEventListener('click', () => {
-  alert('Downloading Flappy Bird for iOS...');
-  // Replace the following URL with the actual download link for Flappy Bird
-  window.location.href = 'https://example.com/flappy-bird.ipa';
-});
